cf66922744d5437dcbc2e11d186221acef336b9c
Marc-Antoine Plourde <56414802+map555@users.noreply.github.com>
2022-03-30 14:15:59 -0400

    Update input loading

---
+++ Utils.py
@@ -0,0 +1,25 @@
+from os.path import exists
+
+
+def IsMemesTextExist():
+    dataset_file_existance = exists("meme_text.txt")
+
+    return dataset_file_existance
+
+def WriteMemesText(df):
+    meme_text = df['content'].str.cat(sep="\n\n")
+    with open("meme_text.txt", "w", encoding='utf-8') as text_file:
+        text_file.write(meme_text)
+        text_file.close()
+
+
+
+def LoadMemesText():
+    memes_text=""
+    with open("meme_text.txt","r",encoding="utf-8") as text_file:
+        memes_text = text_file.read()
+        text_file.close()
+
+    return memes_text
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from DatasetBuilder import *
+from Utils import *
 
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', 1500)
@@ -14,11 +15,12 @@ if __name__ == '__main__':
         df = importDataset()
     else:
         df = getDataset("ImgFlip575K_Dataset-master\\dataset\\memes")
-    print("\"Hello\"")
-    print(df)
 
-    meme_text = df['content'].str.cat()
+    if IsMemesTextExist():
+        memes_text = LoadMemesText()
+    else:
+        WriteMemesText(df)
+        memes_text = LoadMemesText()
 
-    with open("meme_text.txt", "w", encoding='utf-8') as text_file:
-        text_file.write(meme_text)
+    print("p")
 
